1242c9781de84302bf826de7822e8b3fd030fa06
NguyenThanh <nguyenthanh.php@gmail.com>
2018-02-04 22:40:19 +0700

    E1

---
+++ src/app/app.component.ts
@@ -0,0 +1,38 @@
+import {Component, OnInit, OnDestroy, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import {AppServices} from './services/app.services';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  public selectedUser: any ;
+  public userList = [];
+  public asyncStatus = {
+    loadUserList: false,
+    loadUser: false
+  }
+
+  constructor(private appServices: AppServices) {
+  }
+
+  public ngOnInit() {
+    this.appServices.getAllUsers().then((response: any) => {
+      this.userList = response;
+    });
+  }
+
+  public showUser(user: any) {
+    this.asyncStatus.loadUser = true;
+    this.appServices.getUser(user.login).then((response: any) => {
+      this.selectedUser = response;
+      this.asyncStatus.loadUser = false;
+    });
+  }
+
+  public backToList() {
+    this.selectedUser = null;
+  }
+
+}
---
+++ src/app/services/app.services.ts
@@ -0,0 +1,30 @@
+import {Injectable} from '@angular/core';
+import {Http, Headers, RequestOptions} from '@angular/http';
+
+@Injectable()
+export class AppServices {
+  public readonly baseUrl = 'https://api.github.com';
+
+  constructor(private http: Http) {
+  }
+
+  public getAllUsers() {
+    return new Promise((resolve, reject) => {
+      this.http.get(`${this.baseUrl}/users`)
+        .map(res => res.json())
+        .toPromise()
+        .then(resolve)
+        .catch(reject)
+    })
+  }
+
+  public getUser(name: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.http.get(`${this.baseUrl}/users/${name}`)
+        .map(res => res.json())
+        .toPromise()
+        .then(resolve)
+        .catch(reject)
+    })
+  }
+}
